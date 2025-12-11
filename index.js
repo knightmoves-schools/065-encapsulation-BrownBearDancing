d225ed19bab5bb0fc9eefb75e1d4053f89d53c1d
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-11 11:26:24 -0600

    return method no arguments

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ class Rectangle{
         this.height = height;
     }
     calculateArea(){
-       return this.width * this.height; 
+       return `calculateArea`; 
     }
 }
 
 //should encapsulate the variables and functions within a Rectangle
 //should pass the variables into the Rectangle as constructor arguments
 //should have a calculateArea method that takes no arguments
+
 
 
 
